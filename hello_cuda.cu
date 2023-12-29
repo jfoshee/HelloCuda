328c32e13a50b21203a1b13b63c28f6e8ca540f6
Jacob Foshee <jacobf@gmail.com>
2023-12-29 11:50:05 -0600

    feat(props): output CUDA device properties

--- hello_cuda.cu
+++ hello_cuda.cu
@@ -1,15 +1,25 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-__global__ void helloFromGPU() {
+__global__ void helloFromGPU()
+{
     printf("Hello World from GPU!\n");
 }
 
-int main() {
+int main()
+{
     printf("Hello World from CPU!\n");
 
-    // Launch a kernel on the GPU with one thread.
-    helloFromGPU<<<1, 1>>>();
+    cudaDeviceProp prop;
+    const int device = 0;
+    cudaGetDeviceProperties(&prop, device);
+    printf("Device %d: %s\n", device, prop.name);
+    printf("- Max threads per block: %d\n", prop.maxThreadsPerBlock);
+    printf("- Max block dimensions: (%d, %d, %d)\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+    printf("- Max grid dimensions: (%d, %d, %d)\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+
+    // Launch a kernel on the GPU
+    helloFromGPU<<<2, 2>>>();
 
     // Wait for GPU to finish before accessing on host
     cudaDeviceSynchronize();
